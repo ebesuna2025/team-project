1ac125e31c6b533203b088772e62c6b214886c86
ebesuna2025 <kaeru33@gmail.com>
2025-11-25 21:20:49 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// ボタンをクリックしたときの処理
+document.getElementById("changeBtn").addEventListener("click", function() {
+  // id="message" の要素の文字を変更
+  document.getElementById("message").textContent = "文字が変わりました！🎉";
+});
