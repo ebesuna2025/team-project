032fa35867362456aacebc6498375ed1e3a54dc1
sehreif9694-lab <sehrei.f9694@gmail.com>
2025-11-26 22:29:49 +0900

    scriptを変更しました！

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // ボタンをクリックしたときの処理
 document.getElementById("changeBtn").addEventListener("click", function() {
-  // id="message" の要素の文字を変更
-  document.getElementById("message").textContent = "文字が変わりました！🎉";
+  document.getElementById("message").textContent =
+    "いらっしゃいませ。ゆっくりしていってくださいね ☕";
 });
+
